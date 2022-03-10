4910c7f46afb230113fbacae4449e1b9946a30ba
alexzanderr <alexxander18360@gmail.com>
2022-03-10 14:26:23 +0200

    deploy book ci

---
+++ deploy_book.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/sh
+
+git checkout gh-pages
+
+git checkout main "book/src/*" "book/book.toml"
+
+mv ./book/book.toml ./book/src .
+
+rm -rf book
+
+mdbook build
+
+git add .
+
+git commit -m "rust book built automatically"
+
+git push github gh-pages
+
+echo "deployed book with code=0"
